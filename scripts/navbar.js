f88c2e79b955d5c23ba3e8f712801cfed25438f7
Rin <76119102+Polarx-YZ@users.noreply.github.com>
2023-09-10 21:44:43 -0500

    fixed

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -2,11 +2,11 @@ window.onload = function loadNavBar() {
     const navbar = document.createElement('div');
 
     navbar.innerHTML = `
-    <img id="icon" src="assets/icon.png" aria-label="IA with a Groove Coaster crab head accessory">
+    <img id="icon" src="../assets/icon.png" aria-label="IA with a Groove Coaster crab head accessory">
     <p>Cosmoto</p>
     <div id="navbar-links">
-        <a href="pages/distribution.html">Distribution</a>
-        <a href="index.html">Home</a>
+        <a href="distribution.html">Distribution</a>
+        <a href="../index.html">Home</a>
     </div>
     `;
     navbar.id = "navbar"
